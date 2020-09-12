06ba8721548c22ba882ca8a3c2e15c1558b2bbd1
smm3123 <syedmahdi3123@gmail.com>
2020-09-11 19:37:07 -0500

    Make print message less exciting

--- src/count_down_bot.py
+++ src/count_down_bot.py
@@ -13,7 +13,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(str(client.user) + " has connected to Discord!")
+    print(str(client.user) + " has connected to Discord.")
 
 @client.event
 async def on_message(message):
