643d0daf9083f4999d6794de106294aaba66be3b
smm3123 <syedmahdi3123@gmail.com>
2020-09-11 23:10:45 -0500

    Refactor code into separate methods

--- src/count_down_bot.py
+++ src/count_down_bot.py
@@ -11,24 +11,34 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 
+
+async def do_countdown_message(message):
+    countdown_messages = [
+        "!countdown",
+        "!cd",
+        "!count"
+    ]
+    if message.content.lower() in countdown_messages:
+        await message.channel.send("3\n2\n1\nGo", tts=True)
+
+
+async def do_helicopter_message(message):
+    if message.content.lower() == "!helicopter":
+        tts_msg = "Can you guys hear that helicopter?\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+        await message.channel.send(tts_msg, tts=True)
+
+
 @client.event
 async def on_ready():
     print(str(client.user) + " has connected to Discord.")
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    countdown_messages = [
-        "!countdown",
-        "!cd",
-        "!count"
-    ]
-    if message.content in countdown_messages:
-        await message.channel.send("3\n2\n1\nGo", tts=True)
-    elif message.content == "!helicopter":
-        tts_msg = "Can you guys hear that helicopter?\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-        await message.channel.send(tts_msg, tts=True)
+    do_countdown_message(message)
+    do_helicopter_message(message)
 
 client.run(TOKEN)
